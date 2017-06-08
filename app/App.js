05369c25ff7ad7d25ff2cee1267ff26fb9f10ff2
noveogroup-dmeshcheryakov <Dmitriy.Meshcheryakov@noveogroup.com>
2017-06-08 10:44:10 +0600

    Add babel-preset-stage-3

--- app/App.js
+++ app/App.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
+const obj1 = {
+  first: 1,
+  second: '2',
+};
+
+const obj2 = {
+  ...obj1,
+  second: '3',
+};
+
+console.log(obj1, obj2);
+
 const App = () => (
   <div className="test">Hello, World!</div>
 );
